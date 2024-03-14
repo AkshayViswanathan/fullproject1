b420039624d588d36467c2dbcaa28c04982690ca
AkshayViswanathan <akshayviswanathan@techversantinfotech.com>
2024-03-14 18:34:00 +0530

    updated

--- src/components/Dashboard/AdminDashBoardMain.vue
+++ src/components/Dashboard/AdminDashBoardMain.vue
@@ -1,107 +1,180 @@
-  <template>
-      <div class="">
-        <NavBar @toggleEmit="sidebartoggle" />
-          <div class="flex">
-            <div :class="{ 'sidebar-open': isToggle, 'sidebar-closed': !isToggle }" class="sidebar h-screen bg-gray-800 overscroll-auto text-gray-400 ">
-              <div class="m-8">
-            <label class=""><i class="fa-solid fa-briefcase"></i> Admin Dashboard</label>
-        </div>
-      </div>
-      
-      <div class="DashboardBody bg-gray-300 h-screen w-screen flex pt-[100px] items-center flex-col border-2	border-gray-300">
-          
-      <div class="bg-white w-[1100px] rounded-md border-2	border-gray-300">
-        <div class="w-full bg-green-50 h-[60px] text-gray-900 p-3 text-xl rounded-md"><i class="fa-solid fa-table"></i> Data Table </div>
-        <div class="flex gap-4 h-16 border-t-2 border-gray-300	">
-          <p class="pl-6 pt-3">Search:</p>                     
-          <input  type="text" class="mt-2 h-10 rounded-md border focus:border-blue-600 outline-none py-2.5  pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.55] sm:text-sm sm:leading-6" placeholder="Enter last name" /> 
-        </div>
-    
+<template>
+  <div class="">
+    <NavBar @toggleEmit="sidebartoggle" />
+    <EditModal :activeModal="activeModal" @closeModal="Modaltogglefunction"  >
 
-  <table class="table">
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>User name</th>
-        <th>Email</th>
-        <th>Role</th>
-        <th>Edit</th>
-        <th>Delete</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(item, index) in allUsers" :key="index">
-        <td>1</td>
-        <td>{{ item?.firstName }} {{  item?.lastName  }}</td>
-        <td>{{ item?.email }}</td>
-        <td>{{ item?.role }}</td>
-        <td><button class="text-white rounded-md max-w-24 bg-blue-600 py-2 px-6">Edit</button></td>
-        <td><button class="text-white rounded-md max-w-24 bg-red-600 py-2 px-6">Delete</button></td>
-      </tr>
-      <!-- Add more rows here if needed -->
-    </tbody>
-  </table>
-
-
-      </div>
+      <div class=" m-2 "  >
         
-          </div>
-        </div>   
+        <div class=" flex flex-col m-2">
+         <label for="firstname" class="text-gray-700  text-sm mb-1"> </label>
+         <input v-model="firstname" v-bind="firstnameAttrs" type="text" class="block w-full rounded-md focus:border-2 ease-in-out duration-100 focus:border-sky-600/[.30] outline-none py-2.5  pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.40] sm:text-sm sm:leading-6" placeholder="Enter first name" />
+        
+        </div>
+         <div class="flex flex-col m-2">
+         <label class="text-gray-700  text-sm mb-1"> Last Name</label>
+         <input v-model="lastname" v-bind="lastnameAttrs" type="text" class="block w-full rounded-md border focus:border-blue-600 outline-none py-2.5  pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.55] sm:text-sm sm:leading-6" placeholder="Enter last name" />
+         </div>
+
+         <div class="flex flex-col w-full m-2 mb-4">
+         <label class="text-gray-700  text-sm mb-1"> Email</label>
+         <input v-model="email" v-bind="emailAtt" type="text" class="block w-full rounded-md focus:border-2 ease-in-out duration-100 focus:border-sky-600/[.30] outline-none py-2.5  pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.40] sm:text-sm sm:leading-6" placeholder="Enter email" />
+         </div>
+         <div class="flex flex-col w-full m-2 mb-4">
+            <label class="text-gray-700 text-sm mb-1">Role</label>
+            <select v-model="role" class="block w-full rounded-md focus:border-2 ease-in-out duration-100 focus:border-sky-600/[.30] outline-none py-2.5 pl-2 pr-8 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.40] sm:text-sm sm:leading-6">
+                <option value="" disabled selected>Select a role</option>
+                <option value="role1">Admin</option>
+                <option value="role2">Agent</option>
+                <option value="role2">Supervisor</option>
+                <option value="role3">QA</option>
+                <option value="role3">Qc</option>
+                <!-- Add more options as needed -->
+            </select>
+         </div>
+
+        <div class="flex justify-between m-2">
+            <button class="text-white rounded-md bg-blue-600 py-2 px-6" @click="$emit('closeModal')"> close </button>
+            <button class="text-white rounded-md bg-emerald-600 py-2 px-6" >Save</button>
+        </div>
+</div>     
+
+
+    </EditModal>
+    
+      <div class="flex">
+        <div :class="{ 'sidebar-open': isToggle, 'sidebar-closed': !isToggle }" class="sidebar h-screen bg-gray-800 overscroll-auto text-gray-400 ">
+          <div class="m-8">
+        <label class=""><i class="fa-solid fa-briefcase"></i> Admin Dashboard</label>
+    </div>
+  </div>
+  
+  <div class="DashboardBody bg-gray-300 h-screen w-screen flex pt-[100px] items-center flex-col border-2	border-gray-300">
+      
+  <div class="bg-white w-[1100px] rounded-md border-2	border-gray-300">
+    <div class="w-full bg-green-50 h-[60px] text-gray-900 p-3 text-xl rounded-md"><i class="fa-solid fa-table"></i> Data Table </div>
+    <div class="flex gap-4 h-16 border-t-2 border-gray-300	">
+      <p class="pl-6 pt-3">Search:</p>                     
+      <input  type="text" class="mt-2 h-10 rounded-md border focus:border-blue-600 outline-none py-2.5  pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.55] sm:text-sm sm:leading-6" placeholder="Enter last name" /> 
+    </div>
+
+
+<table class="table">
+<thead>
+  <tr>
+    <th>#</th>
+    <th>User name</th>
+    <th>Email</th>
+    <th>Role</th>
+    <th>Edit</th>
+    <th>Delete</th>
+  </tr>
+</thead>
+<tbody>
+  <tr v-for="(userlist, index) in listedUser" :key="index">
+    <td>{{ userlist?.id }}</td>
+    <td>{{ userlist?.firstName }} {{ userlist?.lastName  }}</td>
+    <td>{{ userlist?.email }}</td>
+    <td>{{ userlist?.role }}</td>
+    <td><button class="text-white rounded-md max-w-24 bg-blue-600 py-2 px-6" @click="singleserApi(userlist.id)" >Edit</button></td>
+    <td><button class="text-white rounded-md max-w-24 bg-red-600 py-2 px-6" >Delete</button></td>
+  </tr>
+  <!-- Add more rows here if needed -->
+</tbody>
+</table>
+
+
+  </div>
+    
       </div>
-    </template>
-    
-    <script setup>
-    import { ref, onMounted } from "vue";
-    import NavBar from "../NavBar/NavBar.vue"
-    import axios from 'axios';
+    </div>   
+  </div>
+</template>
 
 
-    const allUsers =ref([]) 
-    const isToggle = ref(false);
+
+<script setup>
+import { ref, onMounted } from "vue";
+import NavBar from "../NavBar/NavBar.vue"
+import axios from 'axios';
+import EditModal from './AdminEditModal.vue'
+
+const listedUser =ref([]) 
+const isToggle = ref(false);
+const activeModal = ref(false);
+const singleUser =ref("")
 
 
-    
-    const sidebartoggle = (value) => {
-      isToggle.value = value;
-    
-    };
+const sidebartoggle = (value) => {
+  isToggle.value = value;
 
-    const ListUserApi = async () => {
-      const accessToken = localStorage.getItem('accessToken')
-      console.log(accessToken);
+};
+
+const Modaltogglefunction =()=>{
+activeModal.value = !activeModal.value}
+
+                                                            // all Listed user api call
+const ListUserApi = async () => {
+  const accessToken = localStorage.getItem('accessToken')
+  console.log(accessToken);
 
 
-    try {
-      const apiData = await axios.get('http://localhost:3000/api/admin/users', {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    } ).then(response => response.data);  
-      allUsers.value = apiData.data.users;
-      console.log(allUsers);
+try {
+  const apiData = await axios.get('http://localhost:3000/api/admin/users', {
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+  },
+} ).then(response => response.data);  
+listedUser.value = apiData.data.users;
+  // console.log(allUsers);
 
-    } catch (error) {
-      console.error("fetch failed:", error);
-    }
-  };
+} catch (error) {
+  console.error("fetch failed:", error);
+}
+};
 
-  onMounted(()=>{ListUserApi()} )
+                                                            // singleuser api call
 
-    </script>
-    
-    <style scoped>
-    .sidebar {
-      transition: width 0.5s ease-in-out;
-    }
-    
-    .sidebar-open {
-      width: 250px;
-      max-width: 250px;
-    }
-    
-    .sidebar-closed {
-      width: 0px;
-      /* display: none; */
-    }
-    </style>
-    
+const singleserApi = async (userId) => {
+  const accessToken  = localStorage.getItem('accessToken')
+
+try {
+  const apiData = await axios.get(`http://localhost:3000/api//admin/users/${userId}`, {
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+  },
+} ).then(response => response.data);  
+  singleUser.value = apiData.data.user;
+  console.log(singleUser);
+
+} catch (error) {
+  console.error("fetch failed:", error);
+}
+};
+
+
+
+
+onMounted(()=>{ListUserApi()} )
+
+</script>
+
+
+
+
+
+
+<style scoped>
+.sidebar {
+  transition: width 0.5s ease-in-out;
+}
+
+.sidebar-open {
+  width: 250px;
+  max-width: 250px;
+}
+
+.sidebar-closed {
+  width: 0px;
+  /* display: none; */
+}
+</style>
---
+++ src/components/Dashboard/AdminEditModal.vue
@@ -0,0 +1,50 @@
+<template>
+     <Transition name="outer" >  
+          <div v-show="activeModal" class="absolute w-full bg-black bg-opacity-50 h-screen top-0 left-0 flex justify-center px-8">
+              <div v-if="activeModal" class="p-4 bg-white w-[600px] mt-32  h-[480px] rounded-xl ">
+                 
+              
+
+       <slot />
+  
+              </div>
+          </div>
+  
+      </Transition>
+</template>
+<script setup>
+
+
+defineEmits(["closeModal"])
+
+defineProps({
+    activeModal:{
+        type: Boolean,
+        default: false,
+        
+    },
+    userData: Array
+ 
+})
+
+
+
+
+  </script>
+  
+  <style scoped>
+  
+  .outer-enter-from, .outer-leave-from{
+      opacity: 0;
+  }
+  
+  .outer-enter-active, .outer-leave-active{
+  
+      transition:all 0.5s;
+  }
+  .outer-enter-to, .outer-leave-to{
+      transition: 1;
+  }
+  
+  
+  </style>
