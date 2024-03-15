e6c9221a88a032322bee64cb31afda40b05bd4f0
AkshayViswanathan <akshayviswanathan@techversantinfotech.com>
2024-03-15 18:15:14 +0530

    updated2

---
+++ src/components/Dashboard/AddUsers.vue
@@ -0,0 +1,43 @@
+<template>
+    <Transition name="outer" >  
+         <div v-show="addUserModalOpen" class="absolute w-full bg-black bg-opacity-50 h-screen top-0 left-0 flex justify-center px-8">
+             <div v-if="addUserModalOpen" class="p-4 bg-white w-[600px] mt-32  h-[530px] rounded-xl ">
+               
+      <slot />          <!-- via slot form has been used in adminDashboardMain -->
+
+ 
+             </div>
+         </div>
+ 
+     </Transition>
+</template>
+<script setup>
+
+
+defineEmits(["closeModal"])
+
+defineProps({
+    addUserModalOpen:{
+       type: Boolean,
+       default: false
+   }
+})
+
+ </script>
+ 
+ <style scoped>
+ 
+ .outer-enter-from, .outer-leave-from{
+     opacity: 0;
+ }
+ 
+ .outer-enter-active, .outer-leave-active{
+ 
+     transition:all 0.5s;
+ }
+ .outer-enter-to, .outer-leave-to{
+     transition: 1;
+ }
+ 
+ 
+ </style>
--- src/components/Dashboard/AdminDashBoardMain.vue
+++ src/components/Dashboard/AdminDashBoardMain.vue
@@ -1,46 +1,94 @@
 <template>
+
   <div class="">
     <NavBar @toggleEmit="sidebartoggle" />
-    <EditModal :activeModal="activeModal" @closeModal="Modaltogglefunction"  >
 
-      <div class=" m-2 "  >
-        
-        <div class=" flex flex-col m-2">
-         <label for="firstname" class="text-gray-700  text-sm mb-1">First Name </label>
-         <input v-model="singleUser.firstName"  type="text" class="block w-full rounded-md focus:border-2 ease-in-out duration-100 focus:border-sky-600/[.30] outline-none py-2.5  pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.40] sm:text-sm sm:leading-6" placeholder="Enter first name" />
-        
-        </div>
-         <div class="flex flex-col m-2">
-         <label class="text-gray-700  text-sm mb-1"> Last Name</label>
-         <input v-model="singleUser.lastName" type="text" class="block w-full rounded-md border focus:border-blue-600 outline-none py-2.5  pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.55] sm:text-sm sm:leading-6" placeholder="Enter last name" />
-         </div>
-
-         <div class="flex flex-col w-full m-2 mb-4">
-         <label class="text-gray-700  text-sm mb-1"> Email</label>
-         <input v-model="singleUser.email"  type="text" class="block w-full rounded-md focus:border-2 ease-in-out duration-100 focus:border-sky-600/[.30] outline-none py-2.5  pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.40] sm:text-sm sm:leading-6" placeholder="Enter email" />
-         </div>
-         <div class="flex flex-col w-full m-2 mb-4">
-            <label class="text-gray-700 text-sm mb-1">Role</label>
-            <select  class="block w-full rounded-md focus:border-2 ease-in-out duration-100 focus:border-sky-600/[.30] outline-none py-2.5 pl-2 pr-8 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.40] sm:text-sm sm:leading-6">
-                <option value="" disabled selected>{{ singleUser.role }}</option>
-                <option value="role1">admin</option>
-                <option value="role2">agent</option>
-                <option value="role2">supervisor</option>
-                <option value="role3">QA</option>
-                <option value="role3">QC</option>
-                <!-- Add more options as needed -->
-            </select>
-         </div>
-
-        <div class="flex justify-between m-2">
-            <button class="text-white rounded-md bg-blue-600 py-2 px-6" @click="Modaltogglefunction"> close </button>
-            <button class="text-white rounded-md bg-emerald-600 py-2 px-6" @click="editUserDetailByAdminAPI(singleUser.id)" >Save</button>
-        </div>
-</div>     
-
-
-    </EditModal>
+<!--  user edit modal opening -->
+        <EditModal :activeModal="activeModal" @closeModal="Modaltogglefunction"  >
     
+          <div class=" m-2 "  >
+            
+            <div class=" flex flex-col m-2">
+             <label for="firstname" class="text-gray-700  text-sm mb-1">First Name </label>
+             <input v-model="singleUser.firstName"  type="text" class="block w-full rounded-md focus:border-2 ease-in-out duration-100 focus:border-sky-600/[.30] outline-none py-2.5  pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.40] sm:text-sm sm:leading-6" placeholder="Enter first name" />
+            
+            </div>
+             <div class="flex flex-col m-2">
+             <label class="text-gray-700  text-sm mb-1"> Last Name</label>
+             <input v-model="singleUser.lastName" type="text" class="block w-full rounded-md border focus:border-blue-600 outline-none py-2.5  pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.55] sm:text-sm sm:leading-6" placeholder="Enter last name" />
+             </div>
+    
+             <div class="flex flex-col w-full m-2 mb-4">
+             <label class="text-gray-700  text-sm mb-1"> Email</label>
+             <input v-model="singleUser.email"  type="text" class="block w-full rounded-md focus:border-2 ease-in-out duration-100 focus:border-sky-600/[.30] outline-none py-2.5  pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.40] sm:text-sm sm:leading-6" placeholder="Enter email" />
+             </div>
+             <div class="flex flex-col w-full m-2 mb-4">
+                <label class="text-gray-700 text-sm mb-1">Role</label>
+                <select  class="block w-full rounded-md focus:border-2 ease-in-out duration-100 focus:border-sky-600/[.30] outline-none py-2.5 pl-2 pr-8 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.40] sm:text-sm sm:leading-6">
+                    <option value="" disabled selected>{{ singleUser.role }}</option>
+                    <option value="role1">admin</option>
+                    <option value="role2">agent</option>
+                    <option value="role2">supervisor</option>
+                    <option value="role3">QA</option>
+                    <option value="role3">QC</option>
+                    <!-- Add more options as needed -->
+                </select>
+              </div>
+    
+               <div class="flex justify-between m-2">
+                <button class="text-white rounded-md bg-blue-600 py-2 px-6" @click="Modaltogglefunction"> close </button>
+                <button class="text-white rounded-md bg-emerald-600 py-2 px-6" @click="editUserDetailByAdminAPI(singleUser.id)" >Save</button>
+             </div>
+           </div>     
+        </EditModal> 
+<!--  user edit modal closing -->
+
+<!-- add user modal opening -->
+         <AddUser :addUserModalOpen="addUserModalOpen" @closeModal="Modaltogglefunction"  >
+
+            <div class=" m-2 "  >
+              
+              <div class=" flex flex-col m-2">
+               <label for="firstname" class="text-gray-700  text-sm mb-1">First Name </label>
+               <input v-model="addUserStore.firstName"  type="text" class="block  rounded-md focus:border-2 ease-in-out duration-100 focus:border-sky-600/[.30] outline-none py-2.5  pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.40] sm:text-sm sm:leading-6" placeholder="Enter first name" />
+              
+              </div>
+               <div class="flex flex-col m-2">
+               <label class="text-gray-700  text-sm mb-1"> Last Name</label>
+               <input v-model="addUserStore.lastName" type="text" class="block rounded-md border focus:border-blue-600 outline-none py-2.5  pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.55] sm:text-sm sm:leading-6" placeholder="Enter last name" />
+               </div>
+            
+               <div class="flex flex-col w-full m-2 mb-4">
+               <label class="text-gray-700  text-sm mb-1"> Email</label>
+               <input v-model="addUserStore.email"  type="text" class="block w-full rounded-md focus:border-2 ease-in-out duration-100 focus:border-sky-600/[.30] outline-none py-2.5  pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.40] sm:text-sm sm:leading-6" placeholder="Enter email" />
+               </div>
+               <div class="flex flex-col w-full m-2 mb-4">
+                  <label class="text-gray-700 text-sm mb-1">Role</label>
+                  <select  class="block w-full rounded-md focus:border-2 ease-in-out duration-100 focus:border-sky-600/[.30] outline-none py-2.5 pl-2 pr-8 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.40] sm:text-sm sm:leading-6">
+                      <option value="" disabled selected> {{ addUserStore.role }}</option>
+                      <option value="role1">admin</option>
+                      <option value="role2">agent</option>
+                      <option value="role2">supervisor</option>
+                      <option value="role3">QA</option>
+                      <option value="role3">QC</option>
+                      <!-- Add more options as needed -->
+                  </select>
+               </div>
+
+               <div class="flex flex-col w-full m-2 mb-4">
+               <label class="text-gray-700  text-sm mb-1"> Password</label>
+               <input v-model="addUserStore.password"  type="text" class="block w-full rounded-md focus:border-2 ease-in-out duration-100 focus:border-sky-600/[.30] outline-none py-2.5  pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.40] sm:text-sm sm:leading-6" placeholder="password" />
+               </div>
+            
+              <div class="flex justify-between m-2">
+                  <button class="text-white rounded-md bg-blue-600 py-2 px-6" @click="addUserOpenModal"> close </button>
+                  <button class="text-white rounded-md bg-emerald-600 py-2 px-6" @click="registerNewAgentByAdminAPI(singleUser.id)" >Save</button>
+              </div>
+            </div>     
+        </AddUser>  
+<!-- add user modal closing -->
+ 
+  
       <div class="flex">
         <div :class="{ 'sidebar-open': isToggle, 'sidebar-closed': !isToggle }" class="sidebar h-screen bg-gray-800 overscroll-auto text-gray-400 ">
           <div class="m-8">
@@ -51,12 +99,15 @@
   <div class="DashboardBody bg-gray-300 h-screen w-screen flex pt-[100px] items-center flex-col border-2	border-gray-300">
       
   <div class="bg-white w-[1100px] rounded-md border-2	border-gray-300">
-    <div class="w-full bg-green-50 h-[60px] text-gray-900 p-3 text-xl rounded-md"><i class="fa-solid fa-table"></i> Data Table </div>
+    <div class="w-full bg-green-50 h-[60px] text-gray-900 p-3 text-xl rounded-md flex justify-between">
+     <div>  <i class="fa-solid fa-table"></i> Data Table  </div>
+      <button @click="addUserOpenModal" class="bg-green-800 w-[100px] h-[35px] text-white rounded-md"> <i class="fa-solid fa-user-plus"> Add</i></button>
+    </div>
+   
     <div class="flex gap-4 h-16 border-t-2 border-gray-300	">
       <p class="pl-6 pt-3">Search:</p>                     
       <input  type="text" class="mt-2 h-10 rounded-md border focus:border-blue-600 outline-none py-2.5  pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600/[.55] sm:text-sm sm:leading-6" placeholder="Enter last name" /> 
     </div>
-
 
 <table class="table">
 <thead>
@@ -76,7 +127,7 @@
     <td>{{ userlist?.email }}</td>
     <td>{{ userlist?.role }}</td>
     <td><button class="text-white rounded-md max-w-24 bg-blue-600 py-2 px-6" @click="singleserApi(userlist.id)" >Edit</button></td>
-    <td><button class="text-white rounded-md max-w-24 bg-red-600 py-2 px-6" >Delete</button></td>
+    <td><button class="text-white rounded-md max-w-24 bg-red-600 py-2 px-6" @click="deleteUserDetailByAdminAPI(userlist.id)" >Delete</button></td>
   </tr>
   <!-- Add more rows here if needed -->
 </tbody>
@@ -94,10 +145,13 @@ import { ref, onMounted } from "vue";
 import NavBar from "../NavBar/NavBar.vue"
 import axios from 'axios';
 import EditModal from './AdminEditModal.vue'
+import AddUser from './AddUsers.vue'
 
+const addUserModalOpen = ref(false)
 const listedUser =ref([]) 
 const isToggle = ref(false);
 const activeModal = ref(false);
+
 const singleUser =ref(
  {
   id:"",  
@@ -108,6 +162,20 @@ firstName: "",
  }
 )
 
+const addUserStore = ref({
+  id:"",  
+firstName: "",
+  lastName:"",
+  role:"",
+  email:"",
+ 
+})
+
+
+const addUserOpenModal =()=>{
+  addUserModalOpen.value = !addUserModalOpen.value
+console.log(addUserModalOpen);
+}
 
 
 
@@ -193,6 +261,52 @@ location.reload();
 };
 
 
+                                           //  delete userlist api call
+
+const deleteUserDetailByAdminAPI = async (userId) => {
+  const accessToken  = localStorage.getItem('accessToken')
+
+try {
+   await axios.delete(`http://localhost:3000/api//admin/users/${userId}`, {
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+  },registerNewAgentByAdminAPI
+} ).then(response => response.data);  
+
+activeModal.value= false
+location.reload();
+
+} catch (error) {
+  console.error("delete api method failed:", error);
+}
+};
+
+
+                                 // register a new agent by Admin
+
+const registerNewAgentByAdminAPI = async (userId) => {
+  const accessToken  = localStorage.getItem('accessToken')
+
+try {
+   await axios.post(`http://lo  calhost:3000/api//admin/users/${userId}`,{
+    "firstName": addUserStore.value.firstName,
+  "lastName": addUserStore.value.lastName,
+  "email": addUserStore.value.email,
+  "role": addUserStore.value.role,
+  "password": addUserStore.value.password,
+   } ,{
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+  },
+} ).then(response => response.data);  
+
+
+location.reload();
+
+} catch (error) {
+  console.error("delete api method failed:", error);
+}
+};
 
 
 onMounted(()=>{ListUserApi()} )
